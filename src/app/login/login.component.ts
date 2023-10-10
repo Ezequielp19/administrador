f59312ec64ab51df45be82e26c1c41e3744f96bd
ezequiel <ezegonzalez800@gmail.com>
2023-10-10 16:48:17 -0300

    login terminado

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router'; 
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,29 +8,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  navCtrl: any;
 
-  constructor(private router: Router) { }
-
-  ngOnInit() {}
-
-  email: string = '';
-  password: string = '';
+  @ViewChild('emailInput', { static: false }) emailInput: ElementRef;
+  @ViewChild('passwordInput', { static: false }) passwordInput: ElementRef;
 
 
-  login() {
-    
-    if (this.email === 'admin@admin.com' && this.password === 'admin98@2') {
-      
+  constructor(private router: Router,
+    private alertController: AlertController 
+    ){}
+  ngOnInit(){
+  }
+
+  async navigateToPage1() {
+    const email = this.emailInput.nativeElement.value;
+    const password = this.passwordInput.nativeElement.value;
+
+    if (email === 'admin@admin.com' && password === '12345') {
       this.router.navigate(['/home']);
-    } else {
-      
-      console.log('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
-    }
-  }
+    } else { 
+      const alert = await this.alertController.create({
+        header: 'Error de autenticación',
+        message: 'Las credenciales son incorrectas. Por favor, inténtalo de nuevo.',
+        buttons: ['OK']
+      });
 
-  navigateToPage1() {
-    this.router.navigate(['/home']); //
-  }
+      await alert.present();
 
+  }
+   }
 }
